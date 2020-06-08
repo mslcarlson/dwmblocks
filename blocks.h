0742835daa17ac77c871413fbd9bf42222e9cd6c
Matthew Carlson <matt@mcarlson.xyz>
2020-06-07 22:30:06 -0400

    modules now as they were in dwmbar but now with a few clickable actions

--- blocks.h
+++ blocks.h
@@ -1,12 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-        {"", "~/.local/bin/statusbar/archupdates", 1, 1},
+        {"", "~/.local/bin/statusbar/archupdates", 60, 1},
 	{"", "~/.local/bin/statusbar/wifi",	0, 2},
 	{"", "~/.local/bin/statusbar/volume",     0, 3},
-	{"", "~/.local/bin/statusbar/battery",     0, 4},	
-	{"", "~/.local/bin/statusbar/time",     1, 5},
-	{"", "~/.local/bin/statusbar/date", 1, 6},
+	{"", "~/.local/bin/statusbar/battery",     5, 4},	
+	{"", "~/.local/bin/statusbar/time",     5, 5},
+	{"", "~/.local/bin/statusbar/date", 5, 6},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
