e2711a22626a0e56435d4b1a6e72a24fba59945a
Matthew Carlson <matt@mcarlson.xyz>
2020-06-25 17:22:09 -0400

    added some modules but dwmblocks is even more broken now

--- config.h
+++ config.h
@@ -1,12 +1,15 @@
 // Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/* Command */	 /* Update Interval */	/* Update Signal */
-        {"archupdates",  1,                     1},
-	{"wifi",	 0, 			2},
-	{"volume",     	 0, 			3},
-	{"battery",      1, 			4},	
-	{"clock",        1, 			5},
-	{"calendar",     1,                     6},
+        {"archupdates",  0,                     1},
+	{"mail",         0,                     2},
+	{"news",         0,                     3}, 
+	{"wifi",	 0, 			4},
+	{"volume",     	 0, 			5},
+	{"battery",      1, 			6},
+	{"backlight",    0,                     7}, 
+	{"clock",        1, 			8},
+	{"calendar",     1,                     9},
 };
 
 // sets delimeter between status commands. NULL character ('\0') means no delimeter.
