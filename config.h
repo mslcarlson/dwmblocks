cb137cf2a4678aa6ba197fc5123317b05699141f
Matthew Carlson <matt@mcarlson.xyz>
2020-07-16 19:16:15 -0400

    changed modules around

--- config.h
+++ config.h
@@ -1,14 +1,14 @@
 // Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/* Command */	 /* Update Interval */	/* Update Signal */
-        {"archupdates",  0,                     1},
+    {"archupdates",  0,                     1},
 	{"mail",         0,                     2},
 	{"rss",          0,                     3}, 
-	{"inet",	 5, 			4},
-	{"volume",     	 0, 			5},
-	{"battery",      5, 			6},
+	{"inet",	     5, 			        4},
+	{"volume",     	 0, 			        5},
+	{"battery",      5, 			        6},
 	{"backlight",    0,                     7}, 
-	{"clock",        5, 			8},
+	{"clock",        5, 			        8},
 	{"calendar",     5,                     9},
 };
 
