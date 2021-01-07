1eb5aea2ccf71b9da3a46cbdc48b9789c1123a22
Matthew Carlson <matt@mcarlson.xyz>
2021-01-06 23:23:20 -0500

    new modules

--- config.h
+++ config.h
@@ -1,15 +1,16 @@
 // Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/* Command */	 /* Update Interval */	/* Update Signal */
-    {"archupdates",  0,                     1},
-	{"mail",         0,                     2},
-	{"rss",          0,                     3}, 
-	{"network",	     5, 			        4},
-	{"volume",     	 0, 			        5},
-	{"battery",      5, 			        6},
-	{"backlight",    0,                     7}, 
-	{"clock",        5, 			        8},
-	{"calendar",     5,                     9},
+  {"music",      0,                     1},
+  {"upgrades",   0,                     2},
+	{"mail",       0,                     3},
+//{"rss",        0,                     3},
+	{"network",	   5, 			              4},
+	{"vol",        0, 			              5},
+	{"battery",    5, 			              6},
+	{"light",      0,                     7},
+	{"clock",      5, 			              8},
+	{"calendar",   5,                     9},
 };
 
 // sets delimeter between status commands. NULL character ('\0') means no delimeter.
